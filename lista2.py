e2d17acdc9363416da288acfe828c652bb3df71a
Himynameishenrique <hmf@cin.ufpe.br>
2018-09-03 21:43:38 -0300

    Update lista2.py

--- lista2.py
+++ lista2.py
@@ -4,6 +4,8 @@ Q1.
 Primeiro problema: para criar o atributo grau, deve-se localizar o primeiro
 parâmetro maior que zero da esquerda pra direita.
 
+Segundo problema: a derivada de um monômio de coeficiente C e grau N tem como coeficiente c*n e grau n-1. Por exemplo: 4x³ = 12x²
+
 """
 
 class Polinomio:
