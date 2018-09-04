79038bbac3a7a0f6fd425362da3ca01f79435991
Himynameishenrique <hmf@cin.ufpe.br>
2018-09-03 23:53:48 -0300

    Add files via upload

--- lista2.py
+++ lista2.py
@@ -1,12 +1,11 @@
 """
 Q1.
 
-Primeiro problema: para criar o atributo grau, deve-se localizar o primeiro
-parâmetro maior que zero da esquerda pra direita.
-
-Segundo problema: a derivada de um monômio de coeficiente C e grau N tem como coeficiente c*n e grau n-1. Por exemplo: 4x³ = 12x²
+Segundo problema: a derivada de um monômio de coeficiente C e grau N tem como coeficiente c*n e grau n-1.
+Por exemplo: 4x³ = 12x²
 
 """
+import numpy as np
 
 class Polinomio:
 
@@ -14,4 +13,29 @@ class Polinomio:
         self.cfx=list(cfx) #O comando "list(argumento)" converte a tupla em uma lista
         while self.cfx[0]==0: #Deleta os argumentos não válidos
             self.cfx.pop[0]
-        self.grau=len(self.cfx)-1
+        self.grau=len(self.cfx)-1 #Armazena o grau
+
+
+"""
+Q2.
+Primeiro problema: 
+"""
+
+class Matriz:
+    def __init__ (self,linhas,colunas):
+            self.__vetorApoio=np.eye(linhas,colunas) #Como não foi especificado o conteudo da matriz, optei pela identidade
+
+    def __getitem__(self,*argumentos):
+        argumentos=list(argumentos)
+        if len(argumentos)>2 or len(argumentos)<1:
+            raise ValueError ("Argumentos inválidos")
+        elif len(argumentos)==2:
+            linha=argumentos[0]
+            coluna=argumentos
+            [1]
+            return self.__vetorApoio[linha,coluna]
+        else:
+            return self.__vetorApoio[argumentos]
+        
+        
+
